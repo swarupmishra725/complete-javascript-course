c4de08d6b0da4793fdc1d2d0f5fa7b0f7cbc36cc
swarupmishra725 <mishraswarup725@gmail.com>
2023-09-09 11:36:37 +0100

    For the date 09/September/2023

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,7 +5,27 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for(let i=0;i< btnsOpenModal.length;i++){
-    console.log(btnsOpenModal[i].textContent);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click',openModal);
 }
-console.log(10);
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown',function(e){
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});

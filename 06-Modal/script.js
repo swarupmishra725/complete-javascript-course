90e56a63cd3c90a4f31a3805b13156de859366ee
swarupmishra725 <mishraswarup725@gmail.com>
2023-09-09 09:27:02 +0100

    For the date 09/September/2023

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,4 +3,9 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i=0;i< btnsOpenModal.length;i++){
+    console.log(btnsOpenModal[i].textContent);
+}
+console.log(10);
--- 06-Modal/starter/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-
-console.log(10);

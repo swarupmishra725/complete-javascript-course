e7a376bcefcc64f5ceed3de0be54ebd1745611ed
swarupmishra725 <mishraswarup725@gmail.com>
2023-09-20 17:56:26 +0100

    Adding for the day 20/September/2023

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,8 +1,26 @@
 'use strict';
-/*
+/* 
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+ 
+ const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  const  openingHours =  {
+    [weekDays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekDays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekDays[5]]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  };
+
+  
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -10,22 +28,10 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  //ES6 enhanced object literals
+  openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -37,13 +43,83 @@ const restaurant = {
   orderPasta: function(ing1,ing2,ing3){
     console.log(`Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`);
   },
-  orderPizza: function(mainIngridient, ...otherIngredients){
+  orderPizza(mainIngridient, ...otherIngredients){
     console.log(mainIngridient);
     console.log(otherIngredients);
   },
 
 }; 
 
+const properties = Object.keys(openingHours);
+console.log(properties);
+/* 
+console.log(`We are open on ${properties.length} days`);
+
+for(const day of Object.keys(openingHours)){
+  console.log(day);
+}
+
+let openStr = `We are open on ${properties.length} days:`;
+for(const day of properties){
+  openStr += `${day},`;
+}
+console.log(openStr)
+
+
+//Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entry objects
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+//
+for(const [key,{open,close}] of entries){
+  console.log(`On ${key} we opwn at ${open} and close at ${close}`);
+} */
+/* 
+//////////////////////////////////////////////
+//Optional Chaining 
+if(restaurant.openingHours.thu.open) console.log(restaurant.openingHours.thu.open);
+
+//Optiotal chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+//Example
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for(const day of days){
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+
+
+
+//Methods
+console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
+console.log(restaurant.orderRissotato?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.com' }];
+console.log(users[0] ?.name ?? 'User array empty');
+
+//FOR-OF use
+const menu = [...restaurant.starterMenu,...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+
+for(const item of menu.entries()){
+  console.log(`${item[0] + 1}: ${item[1]}`);
+}
+
+for (const [i,el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+ */
+//console.log([...menu.entries()]);
+/*
 const rest1 = {
   name: 'Capri',
   numGuests: 0,
@@ -282,39 +358,68 @@ console.log(i,j,k);
 const [p = 1, q = 2, r =  3] = [8,9];
 console.log(p, q, r); */
 
-//////////////////////////////////////
-// Coding Challenge #4
+
+///////////////////////////////////////
+// Coding Challenge #1
 
 /* 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+We're building a football betting app (soccer for my American friends 😅)!
 
-The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-THIS TEST DATA (pasted to textarea)
-underscore_case
- first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
-underscoreCase      ✅
-firstName           ✅✅
-someVariable        ✅✅✅
-calculateAge        ✅✅✅✅
-delayedDeparture    ✅✅✅✅✅
-
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
-
-Afterwards, test with your own test data!
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 GOOD LUCK 😀
 */
 
-cconst game = {
+/*
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+*/
+
+
+const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
   players: [
@@ -352,5 +457,57 @@ cconst game = {
     team1: 1.33,
     x: 3.25,
     team2: 6.5,
-  },
+  }
 };
+//1
+const [players1,players2] = game.players;
+//2
+const[gk1,...fieldplayers1] = players1;
+const [gk2, ...fieldplayers2] = players2;
+//3
+const allplayers = [...players1,...players2];
+//4
+const players1Final = [...players1,'Thiago','Coutinho','Perisic'];
+//5
+const { odds: { team1, x: draw, team2 }} = game;
+console.log(team1,draw,team2 );
+//6
+  const printGoals= function(...players){
+    console.log(...players);
+    console.log(`${players.length} goals were scored`);
+  };
+
+  printGoals(...game.scored);
+
+  //7
+team1 < team2 && console.log('Team1 is more likely to win');
+team1 > team2 && console.log('Team2 is more likely to win'); 
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+//1
+for(const goal of game)
+console.log();

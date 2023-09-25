8b70aefab3db984b8d35f9fa7ccfbca05d4daed5
swarupmishra725 <mishraswarup725@gmail.com>
2023-09-25 17:13:42 +0100

    Adding for the day 25/September/2023

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -53,23 +53,45 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+/////////////////////////////////////////////////////////
+
+const question = new Map([])
+
+
 /////////////////////////////////////////////////////////
 //MAPS
-const rest = new Map();
+/* const rest = new Map();
 rest.set('name', 'Classio Italiano');
 rest.set(1, 'Fireneze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
-
-rest
-  .set('categories', ['Italoian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('Open', 11)
-  .set('Close', '23');
-
-
+//console.log(rest.set(2, 'Lisbon, Portugal'));
   rest
     .set('categories', ['Italoian', 'Pizzeria', 'Vegetarian', 'Organic'])
-    .set('Open', 11)
-    .set('Close', '23');
+    .set('open', 11)
+    .set('close', 23)
+    .set(true,'We are Open')
+    .set(false,'We are Closed!! :(');
+
+  console.log(rest.get('name'));
+  console.log(rest.get(true));
+  console.log(rest.get(1));
+
+  const time = 8;
+  console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+  console.log(rest.has('categories'));
+  rest.delete(2);
+  //rest.clear();
+  const arr = [1,2]
+  rest.set(arr,'Test'); 
+
+  rest.set(document.querySelector('h1'),'Heading');
+  console.log(rest);
+  console.log(rest.size);
+
+console.log(rest.get(arr)); */
+
+
 /////////////////////////////////////////////
 //Sets
 /* 

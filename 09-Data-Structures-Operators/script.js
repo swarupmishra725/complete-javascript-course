56be791a269610f72fa142b92e80ff5d2c79cff3
swarupmishra725 <mishraswarup725@gmail.com>
2023-09-29 16:19:48 +0100

    Adding for the day 29/September/2023

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,9 +1,19 @@
 'use strict';
 
 // Data needed for a later exercise
-/* const flights =
+const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+//console.log(flights.split('+'));
+const getCode = str => str.slice(0,3).toUpperCase();
+
+for(const flight of flights.split('+')){
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🅾' : ' '}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(44)
+  console.log(output);
+}
+
+/*
 const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekDays[3]]: {
@@ -157,7 +167,7 @@ checkBaggage('I have a Laptop, some food, and a pocket knief');
 checkBaggage('I have clocth, watch and food');
 checkBaggage('I ahave snacks, cloth, a gun'); 
 */
-
+/* 
 console.log('a+very+nice+string'.split('+'));
 console.log('Swarup Mishra'.split(' '));
 
@@ -193,6 +203,21 @@ const maskCreditCard = function(number){
 console.log(maskCreditCard(4596845789246813));
 console.log(maskCreditCard(4596845789245169));
 
+//Repeat Method
+
+const mesage2 = 'Bad weather... All Departures Delayed... \n';
+console.log(mesage2.repeat(5));
+
+
+const planesInLine = function(n){
+  console.log(`There are ${n} planes in line${'✈'.repeat(n)}`);
+}
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+ */
 /////////////////////////////////////////////////////////
 /* 
 const question = new Map([
@@ -259,7 +284,6 @@ rest.set(1, 'Fireneze, Italy');
   console.log(rest.size);
 
 console.log(rest.get(arr)); */
-
 
 /////////////////////////////////////////////
 //Sets
@@ -819,3 +843,53 @@ for(const [min, event] of gameEvents){
   console.log(`[${half} HALF] ${min}: ${event}`);
 }
  */
+//////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click',function(){
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  //console.log(text);
+
+  for(const [i, row] of rows.entries()){
+    const  [first,second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(second[0],second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+})
+*/
+
+/////////////////////////////////////////////////
+
+//String Method Practice

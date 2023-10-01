4f36d227ddfd7543a79c49419001cd078e9abc68
swarupmishra725 <mishraswarup725@gmail.com>
2023-10-01 14:58:08 +0100

    Adding for the day 01/October/2023

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,2 +1,6 @@
 'use strict';
 console.log('Hello');
+console.log('Hello');
+console.log('Hello');
+console.log('Hello');
+console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');console.log('Hello');
